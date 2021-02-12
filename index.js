10e6405af7e329d50ef0949b5e4e252d10fda97d
ankur dixit <ankurdixitd@gmail.com>
2021-02-12 16:49:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function ankurshadowizard(value){
+    let img = document.querySelectorAll('.ankurshadowizard');
+    if (value.shadow == 'hard') 
+        value.shadow = '0px'
+    else
+     value.shadow = '15px'
+      console.log('ankur working ')
+    }
+    
+    module.exports.ankurshadowizard = ankurshadowizard;
